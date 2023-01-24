9ce43e353e06dd54ab37a3f6bba1f09d91fd76ac
cartenzaalief <cartenzaalief@gmail.com>
2023-01-24 13:58:00 +0700

    login

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -10,4 +10,28 @@ module.exports = {
       return res.status(500).send(error);
     }
   },
+  login: async (req, res) => {
+    let { username, password } = req.body;
+
+    try {
+      let data = await UsersModel.findAll({
+        where: {
+          username,
+          password,
+        },
+      });
+
+      if (data.length > 0) {
+        return res.status(200).send(data[0].dataValues);
+      } else {
+        return res.status(200).send({
+          success: false,
+          message: "Account does not exist",
+        });
+      }
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
+    }
+  },
 };
--- src/routers/users.js
+++ src/routers/users.js
@@ -1,7 +1,9 @@
+const { application } = require("express");
 const express = require("express");
 const route = express.Router();
 const { usersController } = require("../controllers");
 
 route.get("/", usersController.getUsersData);
+route.post("/login", usersController.login);
 
 module.exports = route;
